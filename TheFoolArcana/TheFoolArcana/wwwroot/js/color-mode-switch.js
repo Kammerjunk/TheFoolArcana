fed61178eb0460921613cdb9c60a2144eda5a3da
Kammerjunk <kammerjunkie@outlook.com>
2021-08-28 18:13:29 +0200

    switch to dark mode bootstrap

---
+++ TheFoolArcana/TheFoolArcana/wwwroot/js/color-mode-switch.js
@@ -0,0 +1,7 @@
+﻿if (window.matchMedia("(prefers-color-scheme: dark)").media === "not all") {
+    document.documentElement.style.display = "none";
+    document.head.insertAdjacentHTML(
+        "beforeend",
+        "<link rel=\"stylesheet\" href=\"~/lib/bootstrap-dark-5/dist/css/bootstrap-night.min.css\" media=\"(prefers-color-scheme: dark)\" />"
+    );
+}
