f559495293a8397d7e9ffea1f433f4a653a57255
Kammerjunk <kammerjunkie@outlook.com>
2021-08-29 00:35:27 +0200

    add options pattern for social links

--- TheFoolArcana/TheFoolArcana/Controllers/SocialLinksController.cs
+++ TheFoolArcana/TheFoolArcana/Controllers/SocialLinksController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TheFoolArcana.Models.Options;
 using TheFoolArcana.ViewModels;
 
 namespace TheFoolArcana.Controllers
 {
     public class SocialLinksController : Controller
     {
+        private SocialLinksOptions Options { get; }
+
+        public SocialLinksController(IConfiguration config)
+        {
+            Options = new SocialLinksOptions();
+            config.GetSection(SocialLinksOptions.SocialLinks).Bind(Options);
+        }
+
         public IActionResult Index()
         {
 
-            var vm = new SocialLinksIndexViewModel(5); //TODO config
+            var vm = new SocialLinksIndexViewModel(Options.MaxCardsPerRow); //TODO config
             return View(vm);
         }
     }
---
+++ TheFoolArcana/TheFoolArcana/Models/Options/SocialLinksOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheFoolArcana.Models.Options
+{
+    public class SocialLinksOptions
+    {
+        public const string SocialLinks = "SocialLinks";
+
+        public int MaxCardsPerRow { get; set; }
+    }
+}
