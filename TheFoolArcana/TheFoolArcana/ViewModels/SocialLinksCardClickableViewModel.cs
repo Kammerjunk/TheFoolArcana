178e88a47372e3308680aa22b7c2e17b838a0452
Kammerjunk <kammerjunkie@outlook.com>
2021-08-28 23:00:38 +0200

    basic SL index

---
+++ TheFoolArcana/TheFoolArcana/ViewModels/SocialLinksCardClickableViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheFoolArcana.ViewModels
+{
+    public class SocialLinksCardClickableViewModel
+    {
+        public string Arcana { get; set; }
+        public int? Height { get; set; }
+
+        public SocialLinksCardClickableViewModel(string arcana)
+        {
+            Arcana = arcana;
+        }
+    }
+}
